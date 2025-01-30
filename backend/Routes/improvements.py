c699ef0f20dcbdbac5db29cb35722e8a311e8f84
Rishi-Jain2602 <rj1016743@gmail.com>
2025-01-30 20:37:39 +0530

    chng prompt

--- backend/Routes/improvements.py
+++ backend/Routes/improvements.py
@@ -5,7 +5,7 @@ from langchain_core.output_parsers import StrOutputParser
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system","Suggest improvement to the users based on the conversation between AI and user."),
+        ("system","Suggest improvement to the users based on the conversation between AI and user. Add \\n whenever there is change in paragraph or in line in your response"),
         MessagesPlaceholder(variable_name="history"),
         ("human","You need to suggest me improvements based on my conversation with you or ai bot")
     ]
@@ -15,10 +15,6 @@ chain = prompt | llm | StrOutputParser()
 def improvements():
     if not memory.chat_memory.messages:
         return "No conversation history available."
-    
-    # if memory.chat_memory.messages and memory.chat_memory.messages[-1].type == "ai":
-    #     # Remove the last AI message if it exists
-    #     memory.chat_memory.messages.pop()
     response = chain.invoke(
         {
             "history":memory.chat_memory.messages,
