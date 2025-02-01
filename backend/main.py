85d2478733bf3a83d698239d706fbb458b0da8e7
Rishi-Jain2602 <rj1016743@gmail.com>
2025-02-01 18:32:03 +0530

    Adding apis

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from Routes.reply import reply_user
+from Routes.talkAI import reply_user
 from Routes.summary import summarization
 from Routes.improvements import improvements
-
+from Routes.withAI import Analyze_users
+from Model.memory import memory
 app = FastAPI()
 
 app.add_middleware(
@@ -15,13 +16,24 @@ app.add_middleware(
     allow_headers=["*"],  
 )
 
-class textInput(BaseModel):
+class talkAIInput(BaseModel):
     transcript: str
 
-@app.post("/convai/chat")
-def voice_analyzer(text:textInput):
+@app.post("/convai/talkAI")
+def voice_analyzer(text:talkAIInput):
     audio_transcript = text.transcript
     response = reply_user(audio_transcript)
+    return response
+
+class withAIInput(BaseModel):
+    user1: str
+    user2: str
+
+@app.post("/convai/withAI")
+def voice_analyzer(query:withAIInput):
+    user1 = query.user1
+    user2 = query.user2
+    response = Analyze_users(user1,user2)
     return response
 
 @app.get("/convai/summary")
@@ -33,6 +45,14 @@ def conv_summary():
 def conv_summary():
     response = improvements()
     return response
+
+@app.get("/convai/memory")
+def clear_memory():
+    if not memory.chat_memory.messages:
+        return "Memory is already cleared"
+    
+    memory.chat_memory.clear()
+    return "memory is cleared"
 
 
 if __name__ == "__main__":
