d8edd91daf2a32190ba0883c9e671f0cc98917bd
Rishi-Jain2602 <rj1016743@gmail.com>
2025-02-01 18:31:23 +0530

    Summary of conv.

--- backend/Routes/summary.py
+++ backend/Routes/summary.py
@@ -1,11 +1,14 @@
-from Routes.reply import memory
+from Model.memory import memory
 from Model.mistral import llm
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
 
+f = open("Routes/Prompts/Summary.txt", "r")
+summary_prompt = f.read()
+
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system","You are provided with conversation history of user and AI Bot. Your job is to summarize the Conversation."),
+        ("system",f"{summary_prompt}"),
         MessagesPlaceholder(variable_name="history"),
         ("human", "Please provide a summary of the above conversation.")
     ]
@@ -13,6 +16,8 @@ prompt = ChatPromptTemplate.from_messages(
 chain = prompt | llm | StrOutputParser()
 
 def summarization():
+    if not memory.chat_memory.messages:
+        return "You haven't had any conversations with AI."
     response = chain.invoke(
         {
             "history":memory.chat_memory.messages,
